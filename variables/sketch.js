003122d6b21e2881badd1a5b1e79f011a562f634
nexusbug <nexusbug@users.noreply.github.com>
2017-08-26 21:24:01 -0400

    variables

---
+++ variables/sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+    createCanvas(600, 400);
+    background(250, 250, 100);
+
+}
+
+function draw() {
+    fill(250, 100, 100);
+    noStroke();
+    ellipse(mouseX, mouseY, 20, 20);
+
+}
+
+function mousePressed() {
+    background(250, 250, 100);
+}
