2a8fb52bdf9e97311fdb65fd1f0fa5326e267241
nexusbug <nexusbug@users.noreply.github.com>
2017-10-08 09:37:38 -0400

    let, var, const

---
+++ 16_topics_of_JS_ES6/16_01/sketch.js
@@ -0,0 +1,49 @@
+/*
+Website
+http://nexusbug.com/
+
+github Repo
+https://github.com/nexusbug/Code_365
+
+About
+Doing a challenge, coding three sixty five
+There I hop on the train of Mr. Shiffman
+And code along rainbow colored railway
+One video each day, keeps the gray away
+
+Coding Train
+Daniel Shiffman
+https://www.youtube.com/user/shiffman/
+https://www.patreon.com/codingtrain
+
+Framework
+p5js - A beautiful Javascript library
+https://p5js.org/
+
+this.day
+45th
+2017 October 08
+
+this.videos
+16.1: let vs var - Topics of JavaScript/ES6
+https://youtu.be/q8SHaDQdul0
+
+16.2: const - Topics of JavaScript/ES6
+https://youtu.be/2iLVFyYwyRA
+*/
+
+
+
+function setup() {
+  noCanvas();
+console.log(x);
+var x = 100;
+createP(x);
+
+let y = 40;
+createP(y);
+
+const particle = {x:200, y:100};
+createP(particle.x);
+createP(particle.y);
+}
