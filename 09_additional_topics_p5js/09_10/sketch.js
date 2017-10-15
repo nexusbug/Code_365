088fbfed83f7ce52ba73ad6b86acab9ec95e7a6f
nexusbug <nexusbug@users.noreply.github.com>
2017-10-15 10:08:20 -0400

    reset a sketch - p5js - day 52

---
+++ 09_additional_topics_p5js/09_10/sketch.js
@@ -0,0 +1,83 @@
+let btn;
+
+
+class Circle {
+  constructor() {
+    this.x = random(width);
+    this.y = random(height);
+    this.r = random(12, 48);
+  }
+}
+
+function setup() {
+  createCanvas(350, 350);
+  createP("");
+  btn = createButton('reset');
+  btn.style('font-size', '16px');
+  resetSketch();
+
+  btn.mousePressed(resetSketch);
+
+
+}
+
+function resetSketch() {
+  background(255);
+  let circles = [];
+  let counter = 0;
+  while (circles.length < 1000) {
+    let overlapping = false;
+    let proposalCircle = new Circle;
+    for (let j = 0; j < circles.length; j++) {
+      let existingCircle = circles[j];
+      let d = dist(proposalCircle.x, proposalCircle.y, existingCircle.x, existingCircle.y);
+      if (d < proposalCircle.r + existingCircle.r) {
+        overlapping = true;
+        break;
+      }
+    }
+
+    if (!overlapping) {
+      circles.push(proposalCircle);
+      noStroke();
+      fill(0, 255, 250, 100);
+      ellipse(proposalCircle.x, proposalCircle.y, proposalCircle.r * 2, proposalCircle.r * 2);
+    }
+
+    counter++;
+    if (counter > 100000) {
+      break;
+    }
+  }
+}
+
+/*
+Website
+http://nexusbug.com/
+
+github Repo
+https://github.com/nexusbug/Code_365
+
+About
+Doing a challenge, coding three sixty five
+There I hop on the train of Mr. Shiffman
+And code along rainbow colored railway
+One video each day, keeps the gray away
+
+Coding Train
+Daniel Shiffman
+https://www.youtube.com/user/shiffman/
+https://www.patreon.com/codingtrain
+
+Framework
+p5js - A beautiful Javascript library
+https://p5js.org/
+
+this.day
+52th
+2017 October 15
+
+this.video
+9.10: Reset a Sketch with Button Click - p5.js Tutorial
+https://youtu.be/lm8Y8TD4CTM
+*/
