8982f4664798888ec9f0edf07d255e6a910d32f7
nexusbug <nexusbug@users.noreply.github.com>
2017-12-08 14:03:52 -0500

    spiral exp 2

--- exps/spiral_exp/sketch.js
+++ exps/spiral_exp/sketch.js
@@ -1,40 +1,76 @@
 let slider;
 
-let f;
+let img;
+
 
 let a = 1;
 let b = 0.5;
 let n = 4;
-let c = log(b * tan(f/(2 * n)));
+let c;
+
+let rot = 0;
+
+function preload() {
+  img = loadImage("wp.png");
+}
 
 function setup() {
-  createCanvas(400, 400);
-  slider = createSlider(0,360,45,15);
+  createCanvas(600, 600);
+  //slider = createSlider(0,360,45,15);
+
 }
 
 
 function draw() {
-  //angleMODE(DEGREES);
+  //angleMode(DEGREES);
   background(51);
   translate(width / 2, height / 2);
   stroke(255);
+  strokeWeight(4);
   noFill();
 
+  rotate(rot);
 
-
+  push();
+  rotate(100);
+  image(img, -200, -200, 400, 400);
+  pop();
 
   beginShape();
-  for (let angle = 0; angle < TWO_PI; angle += 0.1) {
+  for (let angle = 0; angle < 0.9 * TWO_PI; angle += 0.01) {
 
-    f = angle;
 
-    let na = 2 / n;
-    let x = c * cos(angle);
-    let y = c * sin(angle);
+    c = log(b * tan(angle / (2 * n)));
+
+    let r = 120 * (a / c);
+
+    let x = r * -cos(angle);
+    let y = r * -sin(angle);
 
     vertex(x, y);
   }
-  endShape(CLOSE);
+
+  endShape();
+
+  beginShape();
+  for (let angle = 0; angle < 0.9 * TWO_PI; angle += 0.01) {
+
+
+    c = log(b * tan(angle / (2 * n)));
+
+    let r = 120 * (a / c);
+
+    let x = r * cos(angle);
+    let y = r * -sin(-angle);
+
+
+    vertex(x, y);
+  }
+
+  endShape();
+
+
+  rot +=0.01;
 
 }
 
