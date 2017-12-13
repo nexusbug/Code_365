a7ad1aeca0dc2ab8f53e26fdadaf6988d44e0626
nexusbug <nexusbug@users.noreply.github.com>
2017-12-13 10:13:11 -0500

    spiral exp done

--- exps/spiral_exp/sketch.js
+++ exps/spiral_exp/sketch.js
@@ -1,72 +1,194 @@
 let wW, wH, cW, cH;
-let canvas;
-let sp;
 
-let img;
+let cnv;
 
-let galaxy;
-let ga2;
+
+let mwArm1; //milkyway arm 1
+let mwArm2; //milkyway arm 2
+let wp; //whirlpool
+let ngc13;
+let ngc47;
+
+let space;
 
 let rot = 0;
 
+
+
 function preload() {
-  img = loadImage("wpb.png");
+
 }
 
 function setup() {
   calcCanvasSize();
-  canvas = createCanvas(cW, cH);
+  cnv = createCanvas(cW, cH);
 
+  space = new Space();
 
-  galaxy = new Whirlpool(1);
-  ga2 = new Whirlpool(1);
+  mwArm1 = new Galaxy(0.5, 0.5, 4, 30, 0.03, 0.9, 120);
+  mwArm2 = new Galaxy(0.5, 0.5, 4, 20, 0.03, 0.9, 120);
 
-  sp = new Space();
+  wp = new Galaxy(0.5, 0.5, 4, 20, 0.03, 0.9, 100);
+
+  ngc13 = new Galaxy(0.3, 1, 2, 12, 0.03, 0.3, 130);
+
+  ngc47 = new Galaxy(0.6, 3, 2, 10, 0.03, 0.1, 80);
+
 
 }
 
 
 
 function draw() {
-  //angleMode(DEGREES);
   colorMode(HSB, 360, 100, 100);
   imageMode(CENTER);
   background(0);
   translate(cW / 2, cH / 2);
-  stroke(200, 79, 49);
-  strokeWeight(4);
-  noFill();
+  rot = frameCount / 100;
 
-    rotate(frameCount / 200.0);
+  rotate(rot);
 
   push();
   translate(-cW / 2, -cH / 2);
-  sp.stars();
+  space.stars();
   pop();
 
 
 
 
   push();
-  //rotate(-57);
-  image(img, 0, 0, 300, 300);
+  stroke(195, 96, 49);
+  strokeWeight(5);
+  noFill();
+  translate(cW / 4, cH / 4);
+  rotate(-5 * rot);
+  push();
+  mwArm1.disp();
+  pop();
+  rotate(1.60);
+  push();
+  mwArm2.disp();
+  pop();
+  mwArm1.center();
+  pop();
+
+
+  push();
+  stroke(45, 58, 62);
+  strokeWeight(5);
+  noFill();
+  translate(-cW / 4, -cH / 4);
+  rotate(-3 * rot);
+  push();
+  wp.disp();
+  pop();
+  wp.center();
   pop();
 
   push();
-  galaxy.center();
+  stroke(290, 90, 62);
+  strokeWeight(5);
+  noFill();
+  translate(cW / 4, -cH / 4);
+  rotate(-2 * rot);
+  push();
+  ngc13.disp();
+  pop();
+  ngc13.center();
   pop();
 
   push();
-  galaxy.disp();
-  pop();
-
+  stroke(269, 96, 62);
+  strokeWeight(5);
+  noFill();
+  translate(-cW / 4, cH / 4);
+  rotate(-1.5 * rot);
   push();
-  rotate(1.75);
-  ga2.disp();
+  ngc47.disp();
+  pop();
+  ngc47.center();
   pop();
 
 
-  rot -= 0.01;
+}
+
+class Galaxy {
+  constructor(scale, b, n, con1, con2, con3, con4) {
+    this.a = scale;
+    this.b = b;
+    this.n = n;
+    this.c;
+    this.r;
+
+    this.con1 = con1;
+    this.con2 = con2;
+    this.con3 = con3;
+    this.con4 = con4;
+  }
+
+  center() {
+    let l = 50;
+    for (let radius = this.con1; radius >= 10; radius -= 1) {
+      noStroke();
+      fill(59, 90, l);
+      ellipse(0, 0, radius);
+      l += 1;
+    }
+  }
+
+  disp() {
+
+    // let arrcount;
+    // let arr1 = [];
+    // let arr2 = [];
+    // let v;
+
+    let colors = [2, 50, 195, 232, 278];
+
+    beginShape();
+    for (let angle = this.con2; angle < this.con3 * TWO_PI; angle += 0.01) {
+      this.c = log(this.b * tan(angle / (2 * this.n)));
+
+      this.r = this.con4 * (this.a / this.c);
+
+      let x = this.r * -cos(angle);
+      let y = this.r * -sin(angle);
+
+      vertex(x, y);
+      // v = createVector(x, y);
+      // arr1.push(v);
+    }
+    endShape();
+
+    beginShape();
+    for (let angle = this.con2; angle < this.con3 * TWO_PI; angle += 0.01) {
+      this.c = log(this.b * tan(angle / (2 * this.n)));
+
+      this.r = this.con4 * (this.a / this.c);
+
+      let x = this.r * cos(angle);
+      let y = this.r * -sin(-angle);
+
+      // arrcount = floor((angle * 100) - 3);
+
+      vertex(x, y);
+      // v = createVector(x, y);
+      // arr2.push(v);
+    }
+    endShape();
+
+    // Formula for putting stars around arms
+    // cancelled (performance-wise)
+
+    // for (let i = 0; i < arrcount; i++) {
+    //   strokeWeight(1);
+    //   stroke(random(colors), 78, 80);
+    //   fill(random(colors), 78, 80);
+    //   ellipse(arr1[i].x + random(-20, 20), arr1[i].y + random(-20, 20), random(0, 0.4));
+    //   ellipse(arr2[i].x + random(-20, 20), arr2[i].y + random(-20, 20), random(0, 0.4));
+    // }
+
+  }
 
 }
 
@@ -77,7 +199,7 @@ class Space {
   }
 
   stars() {
-    noStroke();;
+    noStroke();
     randomSeed(4);
     for (let i = 0; i < 300; i++) {
       this.xp = random(cW + 200);
@@ -85,61 +207,6 @@ class Space {
       fill(random(360), 85, 78);
       ellipse(this.xp, this.yp, 2);
     }
-  }
-
-}
-
-class Whirlpool {
-  constructor(scale) {
-    this.a = scale;
-    this.b = 0.5;
-    this.n = 4;
-    this.c;
-    this.r;
-
-
-  }
-
-  center() {
-    let l = 70;
-    for (let radius = 40; radius >= 10; radius -= 1) {
-      noStroke();
-      fill(59, 90, l);
-      ellipse(0, 0, radius);
-      l += 1;
-    }
-  }
-
-  disp() {
-    beginShape();
-    for (let angle = 0.03; angle < 0.9 * TWO_PI; angle += 0.01) {
-      this.c = log(this.b * tan(angle / (2 * this.n)));
-
-      this.r = 120 * (this.a / this.c);
-
-      let x = this.r * cos(angle);
-      let y = this.r * -sin(angle);
-
-      vertex(x, y);
-    }
-    endShape();
-
-    beginShape();
-    for (let angle = 0.03; angle < 0.9 * TWO_PI; angle += 0.01) {
-      this.c = log(this.b * tan(angle / (2 * this.n)));
-
-      this.r = 120 * (this.a / this.c);
-
-      let x = this.r * -cos(angle);
-      let y = this.r * -sin(-angle);
-
-      vertex(x, y);
-    }
-    endShape();
-
-
-
-
   }
 
 }
