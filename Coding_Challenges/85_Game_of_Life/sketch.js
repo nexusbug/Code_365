51d4ed26cbfa71f5ade3d2e48f19bfc75eba2104
nexusbug <nexusbug@users.noreply.github.com>
2018-01-15 13:11:11 -0500

    Game of Life

---
+++ Coding_Challenges/85_Game_of_Life/sketch.js
@@ -0,0 +1,110 @@
+let grid;
+let cols;
+let rows;
+let resolution = 10;
+
+function make2DArray(cols, rows) {
+  let arr = new Array(cols);
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = new Array(rows);
+  }
+  return arr;
+}
+
+
+function setup() {
+  createCanvas(600, 600);
+  cols = width / resolution;
+  rows = height / resolution;
+
+  grid = make2DArray(cols, rows);
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      grid[i][j] = floor(random(2));
+    }
+  }
+}
+
+
+
+function draw() {
+  background(0);
+
+  stroke(0);
+
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      let x = i * resolution;
+      let y = j * resolution;
+      if (grid[i][j] == 1) {
+        fill(255);
+        rect(x, y, resolution - 1, resolution - 1);
+      }
+    }
+  }
+
+  let next = make2DArray(cols, rows);
+
+  // compute next based on grid
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      let state = grid[i][j];
+
+      //count live neighbours
+      let sum = 0;
+      let neighbours = countNeighbours(grid, i, j);
+
+      if (state == 0 && neighbours == 3) {
+        next[i][j] = 1;
+      } else if (state == 1 && (neighbours < 2 || neighbours > 3)) {
+        next[i][j] = 0;
+      } else {
+        next[i][j] = state;
+      }
+    }
+  }
+
+  grid = next;
+}
+
+
+function countNeighbours(grid, x, y) {
+  let sum = 0;
+  for (let i = -1; i < 2; i++) {
+    for (let j = -1; j < 2; j++) {
+
+      let col = (x + i + cols) % cols;
+      let row = (y + j + rows) % rows;
+      sum += grid[col][row];
+    }
+  }
+  sum -= grid[x][y];
+  return sum;
+}
+
+/*
+Website
+http://nexusbug.com/
+
+github Repo
+https://github.com/nexusbug/Code_365
+
+About
+Doing a challenge, coding three sixty five
+There I hop on the train of Mr. Shiffman
+And code along rainbow colored railway
+One video each day, keeps the gray away
+
+Coding Train
+Daniel Shiffman
+https://www.youtube.com/user/shiffman/
+https://www.patreon.com/codingtrain
+
+Framework
+p5js - A beautiful Javascript library
+https://p5js.org/
+
+this.video
+Coding Challenge #85: The Game of Life
+https://youtu.be/FWSR_7kZuYg
+*/
