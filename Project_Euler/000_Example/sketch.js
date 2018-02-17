e2dbbf65547610b043fe0888d280dd9f3250bd7c
nexusbug <nexusbug@users.noreply.github.com>
2018-02-17 16:20:21 -0500

    Project Euler - My Answers

---
+++ Project_Euler/000_Example/sketch.js
@@ -0,0 +1,14 @@
+
+
+/*
+Website
+http://nexusbug.com/
+
+github Repo
+https://github.com/nexusbug/
+
+About
+https://projecteuler.net/
+
+Problem 1
+*/
---
+++ Project_Euler/Problem_001/sketch.js
@@ -0,0 +1,22 @@
+let total = 0;
+
+for (let i = 0; i < 1000; i++) {
+  if (i % 3 == 0 || i % 5 == 0) {
+    total += i;
+  }
+}
+
+console.log(total);
+
+/*
+Website
+http://nexusbug.com/
+
+github Repo
+https://github.com/nexusbug/
+
+About
+https://projecteuler.net/
+
+Problem 1
+*/
