77efa6c37125909c173263a23c4b5c480ef11ba3
nexusbug <nexusbug@users.noreply.github.com>
2018-02-23 19:22:27 -0500

    Project 5 NOT Ok

---
+++ Project_Euler/Problem_005/sketch.js
@@ -0,0 +1,29 @@
+// I need a job guys, hello, is anybody listening?
+
+let arr = [1];
+let product = 1;
+
+for (let i = 2; i <= 10; i++) {
+
+  product *= i;
+
+  // if () {
+    arr.push(i);
+  // }
+
+}
+
+console.log(arr);
+
+/*
+Website
+http://nexusbug.com/
+
+github Repo
+https://github.com/nexusbug/
+
+About
+https://projecteuler.net/
+
+Problem 4
+*/
